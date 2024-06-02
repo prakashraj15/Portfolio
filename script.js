777321341098f8cf08406fd398c886c872a07212
prakashraj15 <prakashraj15.tn@gmail.com>
2024-06-01 21:05:21 -0700

    unfinished

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-  
+
+function changeText() {
+    const titles = ["AI Engineer", "ML Engineer","Deep Learning Specialist", "CV Engineer", "NLP Engineer", "Software Engineer"];
+    const randomIndex = Math.floor(Math.random() * titles.length);
+    const jobelement = document.getElementById("job");
+    jobelement.classList.add("fade-out");
+    setTimeout(() => {
+        jobelement.innerHTML = titles[randomIndex];
+        jobelement.classList.remove("fade-out");
+    }, 500);
+}
+
+setInterval(changeText, 3000);
