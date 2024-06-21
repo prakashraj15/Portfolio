66f70afb0ee2ed28caa37660670169b2e095b877
prakashraj15 <prakashraj15.tn@gmail.com>
2024-06-21 02:25:25 -0700

    need to change the achievement and time is not completed!

--- script.js
+++ script.js
@@ -17,3 +17,75 @@ function changeText() {
 }
 
 setInterval(changeText, 3000);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var mainSwiper = new Swiper('.swiper-container--timeline', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination-progressbar',
+        type: 'progressbar',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        slideChange: function () {
+          updateMainPagination(mainSwiper);
+        },
+      },
+    });
+  
+    var nestedSwipers = [];
+  
+    document.querySelectorAll('.nested-swiper').forEach(function (element, index) {
+      nestedSwipers.push(new Swiper(element, {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination-progressbar',
+          type: 'progressbar',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      }));
+    });
+  
+    function updateMainPagination(swiper) {
+      var activeIndex = swiper.activeIndex;
+      var switches = document.querySelectorAll('.swiper-pagination-switch');
+  
+      switches.forEach(function (switchElem, index) {
+        if (index === activeIndex) {
+          switchElem.classList.add('active');
+        } else {
+          switchElem.classList.remove('active');
+        }
+      });
+    }
+  
+    updateMainPagination(mainSwiper);
+  
+    document.querySelectorAll('.swiper-pagination-switch').forEach(function (switchElem, index) {
+      switchElem.addEventListener('click', function () {
+        mainSwiper.slideTo(index);
+      });
+    });
+  });
+
+const achievementImages = ["./assets/ach1.jpg", "./assets/ach2.jpg"];
+let currentAchievementIndex = 0;
+  
+const currentAchievementImage = document.getElementById("currentAchievementImage");
+  
+function updateImage() {
+currentAchievementIndex = (currentAchievementIndex + 1) % achievementImages.length;
+currentAchievementImage.src = achievementImages[currentAchievementIndex];
+  }
+  
+  // Automatically change image every 5 seconds
+setInterval(updateImage, 5000);
+  
